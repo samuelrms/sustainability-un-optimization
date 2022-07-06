b8aceb03072f55f465e0b6f18835b7ea9ecd9d9a
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-07-06 10:21:33 -0300

    feat: Chat implementation in folder structuring

---
+++ src/app/shared/components/Chat/Chat.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ContainerChat } from "./styled";
+
+export const Chat = () => {
+  return <ContainerChat>Chat</ContainerChat>;
+};
--- src/app/shared/components/index.tsx
+++ src/app/shared/components/index.tsx
@@ -2,3 +2,4 @@ export * from "./Container/Container";
 export * from "./Header/Header";
 export * from "./Content/Content";
 export * from "./Button/Button";
+export * from "./Chat/Chat";
